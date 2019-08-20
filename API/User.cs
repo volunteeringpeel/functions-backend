78db801a7664591d0c7f09bb9d838d452148546e
RetroCraft <retrocraft314@gmail.com>
2019-08-20 01:19:55 -0400

    feat(user/GetCurrentUser)

--- API/User.cs
+++ API/User.cs
@@ -42,7 +42,7 @@ namespace VP_Functions.API
         "title", "bio", "pic", "show_exec"
         };
         var reader = await conn.Reader(
-          $@"SELECT TOP 1 [{string.Join("], [", cols)}] FROM [user] WHERE user_id = @id",
+          $@"SELECT TOP 1 [{string.Join("], [", cols)}] FROM [user] WHERE [user_id] = @id",
           new Dictionary<string, object>() { { "id", id } });
         if (reader == null) return Response.Error("Unable to fetch user.", conn.lastError);
 
@@ -59,6 +59,46 @@ namespace VP_Functions.API
         {
           return Response.NotFound("User not found.");
         }
+      }
+      catch (Exception e)
+      {
+        return Response.Error(data: e);
+      }
+      finally
+      {
+        conn.Dispose();
+      }
+    }
+
+    [FunctionName("GetCurrentUser")]
+    public static async Task<IActionResult> GetCurrentUser(
+      [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "me")] HttpRequest req,
+      ClaimsPrincipal principal, ILogger log)
+    {
+      var conn = new FancyConn();
+      try
+      {
+        string email;
+        if ((email = principal?.FindFirst(ClaimTypes.Email)?.Value) == null)
+          return Response.BadRequest("Not logged in.");
+
+        var cols = new List<string>() {
+        "user_id", "role_id",
+        "first_name", "last_name",
+        "email", "phone_1", "phone_2", "school",
+        "title", "bio", "pic", "show_exec"
+        };
+        var reader = await conn.Reader(
+          $@"SELECT TOP 1 [{string.Join("], [", cols)}] FROM [user] WHERE [email] = @email",
+          new Dictionary<string, object>() { { "email", email } });
+        if (reader?.HasRows != true) return Response.Error("Unable to fetch user.", conn.lastError);
+
+        reader.Read();
+        var data = new Dictionary<string, object>();
+        for (int i = 0; i < cols.Count; i++)
+          data.Add(cols[i], reader[i]);
+        reader.Close();
+        return Response.Ok("Retrieved user successfully.", data);
       }
       catch (Exception e)
       {
